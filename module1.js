ae9b47cae3e8e786fdca4c87a334e8145da2c62a
Harshit Chandra <chandraharshit714@gmail.com>
2026-03-25 13:58:59 +0530

    msg

--- module1.js
+++ module1.js
@@ -1,9 +1,9 @@
 import {sum,sub,mult,div} from "./module.js"
-let a=sum(65,75)
+let a=sum(6.5,75.75)
 console.log(a)
-let b=sub(8,6)
+let b=sub(8,6.73)
 console.log(b)
-let c=mult(9,7)
+let c=mult(9,7.8)
 console.log(c)
-let d=div(56,2)
+let d=div(97,2)
 console.log(d)
